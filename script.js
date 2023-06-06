1ee12f39f42e706cbbe8296cdfa56c3858a766e6
“Kyle <128731127+markedatbirth@users.noreply.github.com>
2023-06-06 08:55:11 -0400

    create game function

--- script.js
+++ script.js
@@ -10,33 +10,42 @@ function getComputerChoice() {
         return "Paper";
       } else {
         return "Scissors";
-      }
     }
+}
 
     // Plays a single round of Rock Paper Scissors. 
     // The function takes two parameters - the playerSelection and computerSelection 
     // then returns a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
     // playerSelection parameter is case-insensitive (so users can input rock, ROCK, RocK or any other variation).
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
-        return "You Lose! Paper beats Rock";
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors") {
-        return "You Lose! Scissors beats Paper"; 
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
-        return "You Lose! Rock beats Scissors";
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") {
-        return "You Win! Rock beats Scissors"; 
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
-        return "You Win! Paper beats Rock";
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper") {
-        return "You Win! Scissors beats Paper"; 
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Rock") {
-        return "Draw! You both selected Rock"; 
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Paper") {
-        return "Draw! You both selected Paper"; 
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Scissors") {
-        return "Draw! You both selected Scissors"; 
+function game() {
+    let playerSelection = prompt("Rock, Paper, or Scissors?", undefined) 
+    let computerSelection = getComputerChoice();
+
+    function playRound(playerSelection, computerSelection) {
+        playerSelection = playerSelection.toLowerCase();
+        if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
+            return "You Lose! Paper beats Rock";
+        } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors") {
+            return "You Lose! Scissors beats Paper"; 
+        } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
+            return "You Lose! Rock beats Scissors";
+        } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") {
+            return "You Win! Rock beats Scissors"; 
+        } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
+            return "You Win! Paper beats Rock";
+        } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper") {
+            return "You Win! Scissors beats Paper"; 
+        } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Rock") {
+            return "Draw! You both selected Rock"; 
+        } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Paper") {
+            return "Draw! You both selected Paper"; 
+        } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Scissors") {
+            return "Draw! You both selected Scissors"; 
+        }
     }
+    console.log(playRound(playerSelection, computerSelection));
 }
+
+          
+    
